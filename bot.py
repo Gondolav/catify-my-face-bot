b45d89fcc72a9844e03e62ed4374ad8b3ebfaf17
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-07-26 14:18:32 +0200

    Import bot

---
+++ bot.py
@@ -0,0 +1,82 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import logging
+import os
+
+from face_substitution import replace_faces
+
+KEYS = dict([line.split('=') for line in open('.keys')])
+WELCOME_MESSAGE = 'Welcome!\nSend an image with some faces to begin'
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update, context):
+    logger.info('Bot Started')
+    logger.info(f'Keys: {KEYS}')
+    update.message.reply_text(WELCOME_MESSAGE)
+
+
+def help(update, context):
+    update.message.reply_text('Send an image with some faces to begin')
+
+
+def error(update, context):
+    logger.warn(f'Update "{update}" caused error "{context.error}"')
+
+
+def catify(update, context):
+    def remove_image(filename):
+        if os.path.exists(filename):
+            os.remove(filename)
+
+    photos = update.message.photo
+    if photos:
+        user = update.message.from_user
+        update.message.reply_text("I'm working on it...")
+        logger.info(f'Photo received from {user.first_name} {user.last_name}')
+
+        # Download the best photo version
+        photo_id = photos[-1].file_id
+        context.bot.get_file(photo_id).download('image.png')
+
+        if replace_faces('image.png'):
+            # Face substitution was successful
+            update.message.reply_photo(open('image_with_cat.png', 'rb'))
+            remove_image('image_with_cat.png')
+        else:
+            # Face substitution was unsuccessful
+            update.message.reply_text("I didn't find any face!")
+
+        remove_image('image.png')
+
+
+def main():
+    # Initialize the bot
+    updater = Updater(token=KEYS['token'], use_context=True)
+
+    # Get the update dispatcher
+    dp = updater.dispatcher
+
+    # Define command handlers
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', help))
+
+    # Define message handlers
+    dp.add_handler(MessageHandler(Filters.photo, catify))
+
+    # Log all errors
+    dp.add_error_handler(error)
+
+    # Start the bot
+    updater.start_polling()
+
+    # Run the bot until Ctrl-C is pressed or the process receives SIGINT, SIGTERM or SIGABRT
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ face_substitution.py
@@ -0,0 +1,39 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+# Load the cat image
+cat_img = cv2.imread('cat.jpeg')
+
+
+def replace_faces(filename):
+    '''
+    Replaces the faces in the image specified by the given filename with a cat face.
+    
+    Returns true if at least one face was detected and replaced, false otherwise
+    '''
+
+    # Read the image
+    img = cv2.imread(filename)
+
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray)
+
+    if not faces:
+        return False
+
+    # Replace faces with the cat face
+    for (x, y, w, h) in faces:
+        cat_img_resized = cv2.resize(
+            cat_img, (w, h), interpolation=cv2.INTER_AREA)
+        img[y:y+h, x:x+w, :] = cat_img_resized
+
+    # Save the output
+    cv2.imwrite(f'{filename.split(".")[0]}_with_cat.png', img)
+
+    return True
